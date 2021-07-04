4b1ebece4e084e3b9cb9ad0aca5375911bd0a627
BrijeshMondal1 <brijesh140607@gmail.com>
2021-07-04 09:14:46 +0530

    adding some book transaction screen and search screen

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './screens/SearchScreen';
+import BookTransactionScreen from './screens/BookTransactionScreen';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { render } from 'react-dom';
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer></AppContainer>
   );
+  }
 }
 
+const TabNavigator = createBottomTabNavigator({
+
+  Transaction:{screen:BookTransactionScreen},
+  Search:{screen:SearchScreen}
+
+})
+const AppContainer = createAppContainer(
+
+  TabNavigator
+
+)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
