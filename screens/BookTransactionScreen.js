606e02345eb822147b4a7424c8e3b349d37a570c
BrijeshMondal1 <brijesh140607@gmail.com>
2021-07-16 10:36:41 +0530

    changing the icon

--- screens/BookTransactionScreen.js
+++ screens/BookTransactionScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, Image } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Permissions from 'expo-permissions'
 import { TextInput } from 'react-native-gesture-handler';
@@ -9,19 +9,23 @@ export default class BookTransactionScreen extends React.Component {
 
         super()
         this.state={
-            hasCameraPermissions:null,scanned:false,scannedData:'',
-        buttonState:"normal"
+            hasCameraPermissions:null,
+            scanned:false,
+            scannedBookID:'',
+            scannedStudentID:'',
+            buttonState:"normal"
         }
 
     }
 
-    getCameraPermissions=async()=>{
+    getCameraPermissions=async(id)=>{
 
         const {status}=await Permissions.askAsync(Permissions.CAMERA);
         this.setState({
 
             hasCameraPermissions:status==="granted",
-            buttonState:"clicked"
+            buttonState:id,
+            scanned:false
 
         });
 
@@ -29,7 +33,21 @@ export default class BookTransactionScreen extends React.Component {
 
     handleBarcodeScan=async({type,data})=>{
 
-        this.setState({scanned:true,scannedData:data,buttonState:"normal"})
+        const {buttonState}=this.state
+
+        if(buttonState === "BookID"){
+
+            this.setState({scanned:true,
+                scannedBookID:data,
+                buttonState:"normal"})
+
+        }else if(buttonState === "StudentID"){
+
+            this.setState({scanned:true,
+                scannedStudentID:data,
+                buttonState:"normal"})
+
+            }
 
     }
 
@@ -38,7 +56,7 @@ export default class BookTransactionScreen extends React.Component {
         const hasCameraPermissions = this.state.hasCameraPermissions;
         const scanned = this.state.scanned;
         const buttonState = this.state.buttonState;
-        if(buttonState === "clicked" && hasCameraPermissions){
+        if(buttonState !== "normal" && hasCameraPermissions){
 
             return(
 
@@ -52,24 +70,31 @@ export default class BookTransactionScreen extends React.Component {
             return (
 
                 <View style = {styles.container}>
-
+                    <View>
+                        <Image source={require('./assets/booklogo.jpg')} style={{width:200,height:200}}></Image>
+                        <Text style={{textAlign:'center',fontSize:30}}>Willy</Text>
+                    </View>
                     <View style={styles.inputView}>
 
-                        <TextInput style={styles.inputBox}placeholder="Book ID"></TextInput>
+                        <TextInput style={styles.inputBox}placeholder="Book ID" value={this.state.scannedBookID}></TextInput>
                            
                             <TouchableOpacity style={styles.scanButton} 
                             onPress={()=>{this.getCameraPermissions("BookID")}}>
                                 <Text style={styles.buttonText}>Scan</Text>
                             </TouchableOpacity>
+                    </View>
 
-                            <Text style={styles.displayText}>{hasCameraPermissions===true?this.state.scannedData:"Enable the camera permission"}</Text>
+                    <View style={styles.inputView}>
+                   
+                            <TextInput style={styles.inputBox}placeholder="Student ID" value={this.state.scannedStudentID}></TextInput>
                             
                             <TouchableOpacity style={styles.scanButton} 
                              onPress={()=>{this.getCameraPermissions("StudentID")}}>
                                 <Text style={styles.buttonText}>Scan</Text>
                             </TouchableOpacity>
+
+                     </View>
                     
-                    </View>
         
                 </View>
         
@@ -85,6 +110,10 @@ const styles = StyleSheet.create({
 
     container:{flex:1, justifyContent: 'center', alignItems: 'center'},
     displayText: {fontSize: 15, textDecorationLine:'underline'},
-    scanButton:{backgroundColor:'cyan',padding:10, margin:10}
+    scanButton:{backgroundColor:'cyan',padding:10, margin:10},
+    buttonText:{fontSize:15,textAlign:'center'},
+    inputView:{flexDirection:'row',margin:20},
+    inputBox:{width:200,height:30,borderWidth:1.5,fontSize:15},
+    scanButton:{backgroundColor:'cyan',width:50,borderWidth:1.5}
 
 })
